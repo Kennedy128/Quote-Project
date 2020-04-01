abd46264977b781fd4d4e4bc133a0ef597cb80b3
Santa claus <santa@northpole.com>
2020-04-01 12:47:33 +0300

    addition of toggles

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,14 +8,19 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    {id:1, name:'life is what happens when you are busy making plans written by Diana from Linklon',description:'written by Diana from Linklon'},
-    {id:2,name:'You know you are inlove when you cant fall asleep because reality is finally better than your dreams written by Dr Sue Germany',description:'written by Dr Sue Germany'},
-    {id:3,name:'Get busy living or get busy dying written by Mark Caine',description:'written by Mark Claine'},
-    {id:4,name:'twenty years from now you will be more disappointed by the things that you didnt do than the things you did written by Mark Twain',description:'written by Mark Twain'},
-    {id:5,name:'great minds discuss ideas,avarage mind discuss events and small minds discuss people written by Eleanor Roosevelt',description:'written by Eleanor Roosevelt'},
-    {id:6,name:'God is the way ,the truth,and the life whoever believes in him shall inherit the kingdom believed by Kennedy Mbithi',description:'believed by Kennedy Mbithi'},
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Quote(5,'Solve math homework','Damn Math'),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord'),
+
   
   ];
+
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
   constructor() { }
 
